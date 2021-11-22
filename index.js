349130da9106d0e46528427646cbf8891e9aed18
Cole Belfry <cbelfry@student.neumont.edu>
2021-11-22 10:04:31 -0700

    Cookie stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const pug = require('pug');
 const expressSession = require('express-session');
 const path = require('path');
 const routes = require('./routes/routes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -15,6 +16,24 @@ app.use(expressSession({
     saveUninitialized: true,
     resave: true
 }));
+
+app.use(cookieParser('s0m3th1ng'));
+let visited = 0;
+
+app.get('/', (req, res) => {
+    visited++;
+    res.cookie('visited', visited, { maxAge: 99999999999999999999999999999 });
+
+    if (req.cookies.beenHereBefore == 'yes') {
+        //have been before show login page
+        res.send(`Already has account. Visited ${req.cookies.visited} times.`);
+    } else {
+        res.cookie('beenHereBefore', 'yes', { maxAge: 99999999999999 });
+        //have not been before show create user
+        res.send('Users first time on site, give create user');
+        visited = 0;
+    }
+})
 
 const urlencoderParser = express.urlencoded({
     extended: false
--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ const { MongoClient, ObjectId } = require('mongodb');
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
-const dbName = 'Datadb';
+const dbName = 'Userdb';
 const db = client.db(dbName);
-const collection = db.collection('Jokes');
+const collection = db.collection('Users');
 
 exports.index = (req, res) => {
     res.send('This is a sample page, future home of stuff');
